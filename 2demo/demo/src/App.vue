d09d893e832899371a5c231aede3bc454a254a50
lxy <1247839894@qq.com>
2024-10-21 15:09:43 +0800

    1111

--- 2demo/demo/src/App.vue
+++ 2demo/demo/src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
-
+import Header from './components/header.vue';
+import Footer from './components/footer.vue';
 </script>
 
 <template>
+  <Header />
   Akeelah.cn
+  <Footer />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
