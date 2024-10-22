5ec6ced23408c060aba11919c5c8a2193eaad4c5
lxy <1247839894@qq.com>
2024-10-22 16:25:25 +0800

    qqqqq

--- 5demo/element/src/App.vue
+++ 5demo/element/src/App.vue
@@ -43,7 +43,8 @@ const openNotify2 = () => {
   })
 }
 
-import {ref} from 'vue'
+import {ref, reactive} from 'vue'
+import {Delete, Edit, Loading, Plus, Present, Setting, User} from "@element-plus/icons-vue";
 
 //默认选中的菜单索引
 //const selectedIndex = ref("2-2")
@@ -60,6 +61,201 @@ const defaultOpeneds = ref(["1", "3"])
 //用户执行的命令
 const userCommand = (command) => { //点击菜单触发的回调
   console.log("command:", command)
+}
+
+
+//默认选中的标签名称
+const selectedName = ref("2")
+
+//选中标签触发的回调
+const tabClick = (tab, event) => {
+  console.log("tab", tab.props, "event", event)
+}
+
+const tab = reactive({
+  arr: [
+    {name: "1", title: 'Akeelah', content: '内容1'},
+    {name: "2", title: 'Jessika', content: '内容2'},
+    {name: "3", title: 'Berserker.com', content: '内容3'},
+  ]
+})
+
+//添加
+const tabAdd = () => {
+  let index = tab.arr.length
+  index++
+
+  tab.arr.push({
+    name: index,
+    title: '新选项卡' + index,
+    content: '内容' + index
+  })
+}
+
+//移除
+const tabRemove = (name) => {
+  console.log("name:", name)
+
+  const index = tab.arr.findIndex((value) => {
+    return value.name === name
+  })
+
+  tab.arr.splice(index, 1) //移除元素
+}
+
+
+const name = ref('')
+const password = ref('')
+const content = ref('Jessika')
+const url = ref('Akeelah.com')
+const url2 = ref('Berserker')
+const email = ref('123456')
+const selected1 = ref('2')
+
+
+//单选框
+const radio = ref("3")
+const radio2 = ref("b")
+const radio3 = ref("C")
+
+const radioChange = (val) => {
+  console.log("radioChange:", val)
+}
+
+const radioGroupChange = (val) => {
+  console.log("radioGroupChange:", val)
+}
+
+//复选框
+const checked = ref(["1", "2"])
+const checked2 = ref([])
+
+const checkboxGroupChange = (val) => {
+  console.log("checkboxGroupChange", val)
+}
+
+
+const selected11 = ref('2')
+const selected2 = ref('')
+const selected3 = ref('C')
+const selected4 = ref(['A', 'C'])
+
+const data = reactive({
+  options: [
+    {value: 'A', label: '前端',},
+    {value: 'B', label: '后端',},
+    {value: 'C', label: '服务端',}
+  ]
+})
+
+//回调
+const selectChange = (val) => {
+  console.log("selectChange:", val)
+}
+
+
+const date = ref('')
+
+const dateChange = (val) => {
+  console.log("dateChange:", val)
+}
+
+const data1 = ref({
+  name: '',
+  radio: '',
+  checkbox: [],
+  date: '',
+  select: '',
+  multipleSelect: [],
+  textarea: ''
+})
+
+const add = () => {
+  console.log(data1.value)
+}
+
+const reset = () => {
+  data.value = {
+    name: '',
+    radio: '',
+    checkbox: [],
+    date: '',
+    select: '',
+    multipleSelect: [],
+    textarea: ''
+  }
+}
+
+
+const data111 = ref({
+  name: '',
+  radio: '',
+  checkbox: [],
+  date: '',
+  select: '',
+  multipleSelect: [],
+  textarea: ''
+})
+
+const add111 = () => {
+  console.log(data111.value)
+}
+
+const reset111 = () => {
+  data111.value = {
+    name: '',
+    radio: '',
+    checkbox: [],
+    date: '',
+    select: '',
+    multipleSelect: [],
+    textarea: ''
+  }
+}
+
+//对话框
+const dialog = ref(false)
+
+const dialogClose = () => {
+  console.log("关闭")
+}
+
+const currentPage = (val) => {
+  console.log("currentPage:", val)
+}
+
+
+const data222 = ref({
+  arr: [
+    {id: '1', name: 'Akeelah', web: 'Akeelah.com', date: '2023-06-20'},
+    {id: '2', name: 'Jessika', web: 'www.Jessika.com', date: '2023-06-21'},
+    {id: '3', name: 'Berserker', web: 'Berserker.com', date: '2023-06-22'},
+    {id: '4', name: 'Adam', web: 'www.Adam.com', date: '2023-06-22'}
+  ]
+})
+
+//选中的复选框
+let idArr = []
+const selected222 = (data222) => {
+  //console.log("selected", data)
+
+  idArr = [] //重置
+
+  data222.forEach((value) => {
+    idArr.push(value.id)
+  })
+
+  console.log("idArr:", idArr)
+}
+
+//删除
+const del = () => {
+  console.log("del:", idArr)
+}
+
+//编辑
+const edit = (index, row) => {
+  console.log("index:", index, "row:", row)
 }
 </script>
 
@@ -104,27 +300,27 @@ const userCommand = (command) => { //点击菜单触发的回调
   <hr>
   <h3>属性</h3>
   <el-icon size="30" color="red">
-    <Search/>
+    <el-icon-search/>
   </el-icon>
 
   <hr>
   <h3>按钮</h3>
   <el-button type="primary">
     <el-icon>
-      <Search/>
+      <el-icon-search/>
     </el-icon>
     <span> 搜索 </span>
   </el-button>
 
   <el-button type="primary">
     <el-icon>
-      <Search/>
+      <el-icon-search/>
     </el-icon>
   </el-button>
 
   <el-button type="primary" circle>
     <el-icon>
-      <Search/>
+      <el-icon-search/>
     </el-icon>
   </el-button>
 
@@ -158,6 +354,8 @@ const userCommand = (command) => { //点击菜单触发的回调
   <el-button @click="openNotify2">通知2</el-button>
   <hr>
   <hr>
+
+
   <h3>水平导航</h3>
   <el-menu mode="horizontal" :default-active="selectedIndex" @select="selected">
     <el-menu-item index="1">Akeelah</el-menu-item>
@@ -191,7 +389,7 @@ const userCommand = (command) => { //点击菜单触发的回调
     <el-sub-menu index="1">
       <template #title>
         <el-icon>
-          <Search/>
+          <el-icon-search/>
         </el-icon>
         <span>导航一</span>
       </template>
@@ -226,7 +424,7 @@ const userCommand = (command) => { //点击菜单触发的回调
     <el-sub-menu index="1">
       <template #title>
         <el-icon>
-          <Search/>
+          <el-icon-search/>
         </el-icon>
         <span>导航一</span>
       </template>
@@ -244,7 +442,7 @@ const userCommand = (command) => { //点击菜单触发的回调
     <el-sub-menu index="3">
       <template #title>
         <el-icon>
-          <Search/>
+          <el-icon-search/>
         </el-icon>
         <span>导航三</span>
       </template>
@@ -290,6 +488,375 @@ const userCommand = (command) => { //点击菜单触发的回调
       </el-dropdown-menu>
     </template>
   </el-dropdown>
+  <hr>
+  <hr>
+
+  <h3>标签页</h3>
+  <el-tabs v-model="selectedName" @tab-click="tabClick">
+    <el-tab-pane label="Akeelah" name="1">内容1</el-tab-pane>
+    <el-tab-pane label="Jessika" name="2">内容2</el-tab-pane>
+    <el-tab-pane label="Akeelah.com" name="3">内容3</el-tab-pane>
+  </el-tabs>
+
+  <h3>卡片风格</h3>
+  <el-tabs v-model="selectedName" @tab-click="tabClick" type="card">
+    <el-tab-pane label="Akeelah" name="a">内容1</el-tab-pane>
+    <el-tab-pane label="Jessika" name="b">内容2</el-tab-pane>
+    <el-tab-pane label="Jessika.com" name="c">内容3</el-tab-pane>
+  </el-tabs>
+
+  <h3>带有边框的卡片风格</h3>
+  <el-tabs v-model="selectedName" @tab-click="tabClick" type="border-card">
+    <el-tab-pane label="Berserker" name="A">内容1</el-tab-pane>
+    <el-tab-pane label="Adam" name="B">内容2</el-tab-pane>
+    <el-tab-pane label="Berserker.com" name="C">内容3</el-tab-pane>
+  </el-tabs>
+
+  <h3>动态添加</h3>
+  <el-button @click="tabAdd">添加</el-button>
+
+  <el-tabs v-model="selectedName" @tab-remove="tabRemove" closable type="card">
+    <el-tab-pane v-for="(value) in tab.arr" :key="value.name" :label="value.title" :name="value.name">
+      {{ value.content }}
+    </el-tab-pane>
+  </el-tabs>
+  <hr>
+  <hr>
+
+  <div style="width: 300px" >
+    <el-icon>
+      <el-icon-search />
+    </el-icon>
+    <!-- clearable 可一键清空 -->
+    <h3>输入框</h3>
+    <el-input v-model="name" clearable placeholder="请输入用户名"/>
+
+    <!-- show-password 可切换显示隐藏密码 -->
+    <h3>密码框</h3>
+    <el-input v-model="password" show-password placeholder="请输入密码"/>
+
+    <h3>文本域</h3>
+    <el-input type="textarea" v-model="content" rows:2/>
+
+    <h3>输入内容长度限制 - 输入框</h3>
+    <el-input v-model="name" maxlength="10" show-word-limit/>
+
+    <h3>输入内容长度限制 - 文本域</h3>
+    <el-input type="textarea" v-model="content" maxlength="20" rows:3 show-word-limit/>
+
+    <h3>尺寸</h3>
+    大
+    <el-input size="large"/>
+    默认
+    <el-input/>
+    小
+    <el-input size="small"/>
+
+    <h3>前置</h3>
+    <el-input v-model="url">
+      <template #prepend>https://</template>
+    </el-input>
+
+    <h3>后置</h3>
+    <el-input v-model="email">
+      <template #append>@qq.com</template>
+    </el-input>
+
+    <h3>前置后置</h3>
+    <el-input v-model="url2">
+      <template #prepend>https://</template>
+      <template #append>.com</template>
+    </el-input>
+
+    <h3>前置后置扩展 - 搜索</h3>
+    <el-input placeholder="请输入课程名称">
+      <template #prepend>
+        <el-select v-model="selected1" placeholder="请选择" style="width: 100px;">
+          <el-option label="前端" value="1"/>
+          <el-option label="后端" value="2"/>
+          <el-option label="服务端" value="3"/>
+        </el-select>
+      </template>
+      <template #append>
+        <el-button>
+          <el-icon>
+            <el-icon-search/>
+          </el-icon>
+        </el-button>
+      </template>
+    </el-input>
+  </div>
+  <hr>
+  <hr>
+
+
+  <h3>单选框</h3>
+  <el-radio v-model="radio" value="1">前端</el-radio>
+  <el-radio v-model="radio" value="2">后端</el-radio>
+  <el-radio v-model="radio" value="3">服务端</el-radio>
+
+  <h3>单选框 - 事件绑定</h3>
+  <el-radio v-model="radio2" value="a" @change="radioChange">前端</el-radio>
+  <el-radio v-model="radio2" value="b" @change="radioChange">后端</el-radio>
+  <el-radio v-model="radio2" value="c" @change="radioChange">服务端</el-radio>
+
+  <h3>单选框组</h3>
+  <el-radio-group v-model="radio3" @change="radioGroupChange">
+    <el-radio value="A">前端</el-radio>
+    <el-radio value="B">后端</el-radio>
+    <el-radio value="C">服务端</el-radio>
+  </el-radio-group>
+
+  <h3>复选框</h3>
+  <el-checkbox-group v-model="checked">
+    <el-checkbox value="1">前端</el-checkbox>
+    <el-checkbox value="2">后端</el-checkbox>
+    <el-checkbox value="3">服务端</el-checkbox>
+  </el-checkbox-group>
+
+  <h3>事件绑定</h3>
+  <el-checkbox-group v-model="checked2" @change="checkboxGroupChange">
+    <el-checkbox value="1">前端</el-checkbox>
+    <el-checkbox value="2">后端</el-checkbox>
+    <el-checkbox value="3">服务端</el-checkbox>
+  </el-checkbox-group>
+  <hr>
+  <hr>
+
+  <div style="width: 300px;">
+    <h3>下拉框</h3>
+    <el-select v-model="selected11" placeholder="请选择">
+      <el-option value="1" label="前端"/>
+      <el-option value="2" label="后端"/>
+      <el-option value="3" label="服务端"/>
+    </el-select>
+
+    <h3>下拉框 - 事件绑定</h3>
+    <el-select v-model="selected2" @change="selectChange" placeholder="请选择">
+      <el-option value="a" label="前端"/>
+      <el-option value="b" label="后端"/>
+      <el-option value="c" label="服务端"/>
+    </el-select>
+
+    <h3>动态下拉框</h3>
+    <el-select v-model="selected3" placeholder="请选择">
+      <el-option v-for="item in data.options"
+                 :value="item.value"
+                 :label="item.label"
+                 :key="item.value"/>
+    </el-select>
+
+    <h3>多选 - multiple</h3>
+    <el-select v-model="selected4" multiple @change="selectChange" placeholder="请选择">
+      <el-option v-for="item in data.options"
+                 :value="item.value"
+                 :label="item.label"
+                 :key="item.value"/>
+    </el-select>
+  </div>
+  <hr>
+  <hr>
+
+
+  <h3>日期</h3>
+  <el-date-picker v-model="date" type="date" placeholder="请选择"/>
+
+  <h3>日期时间</h3>
+  <el-date-picker v-model="date" type="datetime" placeholder="请选择"/>
+
+  <h3>事件绑定</h3>
+  <el-date-picker v-model="date" type="datetime" value-format="YYYY-MM-DD HH:mm:ss" @change="dateChange"/>
+  <hr>
+  <hr>
+
+
+  <el-form label-width="80" style="width: 400px;">
+    <el-form-item label="文本框">
+      <el-input v-model="data1.name" placeholder="请填写名称"/>
+    </el-form-item>
+
+    <el-form-item label="单选框">
+      <el-radio-group v-model="data1.radio">
+        <el-radio value="1">前端</el-radio>
+        <el-radio value="2">后端</el-radio>
+        <el-radio value="3">服务端</el-radio>
+      </el-radio-group>
+    </el-form-item>
+
+    <el-form-item label="复选框">
+      <el-checkbox-group v-model="data1.checkbox">
+        <el-checkbox value="a">前端</el-checkbox>
+        <el-checkbox value="b">后端</el-checkbox>
+        <el-checkbox value="c">服务端</el-checkbox>
+      </el-checkbox-group>
+    </el-form-item>
+
+    <el-form-item label="日期时间">
+      <el-date-picker v-model="data1.date" type="datetime" value-format="YYYY-MM-DD HH:mm:ss"/>
+    </el-form-item>
+
+    <el-form-item label="下拉框">
+      <el-select v-model="data1.select" placeholder="请选择">
+        <el-option value="A" label="前端"/>
+        <el-option value="B" label="后端"/>
+        <el-option value="C" label="服务端"/>
+      </el-select>
+    </el-form-item>
+
+    <el-form-item label="多选框">
+      <el-select v-model="data1.multipleSelect" multiple placeholder="请选择">
+        <el-option value="AA" label="前端"/>
+        <el-option value="BB" label="后端"/>
+        <el-option value="CC" label="服务端"/>
+      </el-select>
+    </el-form-item>
+
+    <el-form-item label="文本域">
+      <el-input type="textarea" v-model="data1.textarea" rows:2 placeholder="请填写内容"/>
+    </el-form-item>
+
+    <el-form-item>
+      <el-button type="primary" @click="add">添加</el-button>
+      <el-button @click="reset">重置</el-button>
+    </el-form-item>
+  </el-form>
+  <hr>
+  <hr>
+
+
+  <el-button @click="dialog = true">打开</el-button>
+
+  <!-- draggable 允许拖拽 -->
+  <el-dialog v-model="dialog" width="500" title="标题" draggable @close="dialogClose">
+    <el-form label-width="80">
+      <el-form-item label="文本框">
+        <el-input v-model="data111.name" placeholder="请填写名称"/>
+      </el-form-item>
+
+      <el-form-item label="单选框">
+        <el-radio-group v-model="data111.radio">
+          <el-radio value="1">前端</el-radio>
+          <el-radio value="2">后端</el-radio>
+          <el-radio value="3">服务端</el-radio>
+        </el-radio-group>
+      </el-form-item>
+
+      <el-form-item label="复选框">
+        <el-checkbox-group v-model="data111.checkbox">
+          <el-checkbox value="a">前端</el-checkbox>
+          <el-checkbox value="b">后端</el-checkbox>
+          <el-checkbox value="c">服务端</el-checkbox>
+        </el-checkbox-group>
+      </el-form-item>
+
+      <el-form-item label="日期时间">
+        <el-date-picker v-model="data111.date" type="datetime" value-format="YYYY-MM-DD HH:mm:ss"/>
+      </el-form-item>
+
+      <el-form-item label="下拉框">
+        <el-select v-model="data111.select" placeholder="请选择">
+          <el-option value="A" label="前端"/>
+          <el-option value="B" label="后端"/>
+          <el-option value="C" label="服务端"/>
+        </el-select>
+      </el-form-item>
+
+      <el-form-item label="多选框">
+        <el-select v-model="data111.multipleSelect" multiple placeholder="请选择">
+          <el-option value="AA" label="前端"/>
+          <el-option value="BB" label="后端"/>
+          <el-option value="CC" label="服务端"/>
+        </el-select>
+      </el-form-item>
+
+      <el-form-item label="文本域">
+        <el-input type="textarea" v-model="data111.textarea" rows:2 placeholder="请填写内容"/>
+      </el-form-item>
+
+      <el-form-item>
+        <el-button type="primary" @click="add111">添加</el-button>
+        <el-button @click="reset111">重置</el-button>
+      </el-form-item>
+    </el-form>
+  </el-dialog>
+  <hr>
+  <hr>
+
+
+  <h3>page-size:每页显示记录数 total:总记录数</h3>
+  <el-pagination layout="prev, pager, next" :page-size="10" :total="50"/>
+
+  <h3>background:显示背景</h3>
+  <el-pagination layout="prev, pager, next,total" :page-size="5" :total="50" background/>
+
+  <h3>layout="total" 显示总数</h3>
+  <el-pagination layout="prev, pager, next, total" :page-size="5" :total="50"/>
+
+  <h3>layout="jumper" 跳转</h3>
+  <el-pagination layout="prev, pager, next, jumper, total" :page-size="5" :total="50"/>
+
+  <h3>事件绑定</h3>
+  <el-pagination layout="prev, pager, next" :page-size="5" :total="50" @current-change="currentPage"/>
+  <hr>
+  <hr>
+
+
+  <h3>表格</h3>
+  <el-table :data="data222.arr" style="width: 800px;">
+    <el-table-column prop="id" label="编号" width="80"/>
+    <el-table-column prop="name" label="姓名"/>
+    <el-table-column prop="web" label="网站" width="300"/>
+    <el-table-column prop="date" label="日期"/>
+  </el-table>
+
+  <h3>带边框表格</h3>
+  <el-table :data="data222.arr" border style="width: 800px;">
+    <el-table-column prop="id" label="编号" width="80"/>
+    <el-table-column prop="name" label="姓名"/>
+    <el-table-column prop="web" label="网站" width="300"/>
+    <el-table-column prop="date" label="日期"/>
+  </el-table>
+
+  <h3>设置高度固定表头</h3>
+  <el-table :data="data222.arr" border height="120" style="width: 800px;">
+    <el-table-column prop="id" label="编号" width="80"/>
+    <el-table-column prop="name" label="姓名"/>
+    <el-table-column prop="web" label="网站" width="300"/>
+    <el-table-column prop="date" label="日期"/>
+  </el-table>
+
+  <h3>type="selection" 多选</h3>
+  <el-table :data="data222.arr" border style="width: 800px;">
+    <el-table-column type="selection" width="55"/>
+
+    <el-table-column prop="id" label="编号" width="80"/>
+    <el-table-column prop="name" label="姓名"/>
+    <el-table-column prop="web" label="网站" width="300"/>
+    <el-table-column prop="date" label="日期"/>
+  </el-table>
+
+  <h3>按钮</h3>
+  <el-button type="primary" @click="del">删除</el-button>
+
+  <el-table :data="data222.arr" @selection-change="selected222" border style="width: 900px;margin: 3px 0;">
+    <el-table-column type="selection" width="55"></el-table-column>
+
+    <el-table-column prop="id" label="编号" width="80"/>
+    <el-table-column prop="name" label="姓名"/>
+    <el-table-column prop="web" label="网站" width="300"/>
+    <el-table-column prop="date" label="日期"/>
+
+    <el-table-column label="操作" width="150">
+      <template #default="scope">
+        <el-button size="small" type="primary" @click="edit(scope.$index, scope.row)">
+          编辑
+        </el-button>
+        <el-button size="small">删除</el-button>
+      </template>
+    </el-table-column>
+  </el-table>
+
+  <el-pagination layout="prev, pager, next, jumper, total" :page-size="5" :total="50"/>
 </template>
 
 <style scoped>
